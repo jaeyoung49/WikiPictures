6f54d09d6e54e52dcac46734b5eb9b748dd03de3
jaeyoung49 <jaeyoung49@gmail.com>
2016-11-30 09:04:53 +0900

    2016.11.30_09:01
    작성자 : 이재영
    작성내용
    - 변경
    	- BoardDAO.java, BoardDAOImpl, MemberDAO.java, MemberDAOImpl.java
    		- 이동 : model -> dao
    	- MemberService.java, MemberServiceImpl.java
    		- 이동 : model -> service

---
+++ wikipictures/src/main/java/org/kosta/wikipictures/dao/MemberDAO.java
@@ -0,0 +1,11 @@
+package org.kosta.wikipictures.dao;
+
+import org.kosta.wikipictures.vo.MemberVO;
+
+public interface MemberDAO {
+
+	MemberVO login(MemberVO memberVO);
+	public int idcheck(String id);
+	void registerMember(MemberVO vo);
+	public MemberVO findMemberById(String id);
+}
