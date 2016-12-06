d3bae529b6c459e961061e90832e3ffde0ae9489
devjang <devjang2016@gmail.com>
2016-12-06 18:47:14 +0900

    2016.12.06_18:47
    작성자 : 장현석
    작성내용
    - 수정 
    	- datepicker.js
    		충돌수정

--- wikipictures/src/main/webapp/resources/js/datepicker.js
+++ wikipictures/src/main/webapp/resources/js/datepicker.js
@@ -4,11 +4,7 @@ $(document).ready(function(){
     var container=$('.bootstrap-iso form').length>0 ? $('.bootstrap-iso form').parent() : "body";
     date_input.datepicker({
     	language:'kr',
-<<<<<<< HEAD
-        format: 'yyyy-mm-dd',
-=======
         format: 'yyyy/mm/dd',
->>>>>>> branch 'master' of https://github.com/jaeyoung49/WikiPictures
         container: container,
         todayHighlight: true,
         autoclose: true,
