863ec81dd456fbb9c59ac5d797df923abebec7ff
louis205 <louis205@naver.com>
2016-12-13 09:10:43 +0900

    16. 12. 13.
    오저 9시 5분
    작성자 : 명노철
    
    변경
    login_all_fail.jsp 변화없음
    AdminController.java 씨큐리티 좀 적용
    pom.xml 씨큐리티.

--- wikipictures/src/main/java/org/kosta/wikipictures/controller/AdminController.java
+++ wikipictures/src/main/java/org/kosta/wikipictures/controller/AdminController.java
@@ -3,6 +3,7 @@ package org.kosta.wikipictures.controller;
 import javax.annotation.Resource;
 
 import org.kosta.wikipictures.service.AdminService;
+import org.springframework.security.access.annotation.Secured;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
@@ -12,7 +13,9 @@ public class AdminController {
 
 	@Resource
 	private AdminService adminService;
-
+	@Secured("ROLE_MEMBER")
+	// 관리자만 들어가야 할 경우
+	// @Secured({"ROLE_MEMBER","ROLE_ADMIN"})
 	@RequestMapping("show_member_list.do")
 	public ModelAndView memberList(String pageNo) {
 		return new ModelAndView("admin/show_member_list", "listVO", adminService.memberList(pageNo));
@@ -32,4 +35,5 @@ public class AdminController {
 	public ModelAndView sellList(String pageNo) {
 		return new ModelAndView("admin/show_sell_list", "sellList", adminService.sellList(pageNo));
 	}
+
 }
